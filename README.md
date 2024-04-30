355ef6f0cf602faa1bec479eda8a2ab29a7d072f
Tom Nickson <me@tomnickson.com>
2024-04-30 09:58:57 -0700

    init yo

---
+++ README.md
@@ -0,0 +1,73 @@
+# Terminal based training progress looker atter
+
+## wat
+
+it makes nice graphs and stuff and prints them to the terminal for you
+
+## why
+
+* wandb once took 5 minutes to register a run
+* tensorboard is slow and doens't find new runs automatically
+* wandb once took down a training job several hours in because the server shat itself
+* attempting to see trends by performing a running average of printed numbers in my head is surprisingly hard
+
+## how
+
+write a template
+
+```python
+template = """plot(loss, title="Loss", height=2) plot(score, title="Score", height=2)
+
+
+text("Next letter probabilities", bold=True)
+hist1d(probs[0], height=2, labels=letters) hist1d(probs[1], height=2, labels=letters) hist1d(probs[3], height=2, labels=letters)
+hist1d(probs[3], height=2, labels=letters) hist1d(probs[4], height=2, labels=letters) hist1d(probs[5], height=2, labels=letters)
+
+text("Progress", bold=True)
+progress(step, total)"""
+board = TextBoard(template)
+```
+
+then pass it things in your training loop and call print:
+
+```python
+args = {
+'loss': 0,
+'score': 0,
+'probs': 0,
+'letters': string.ascii_lowercase,
+'step': 0,
+'total': len(data)
+}
+
+board = TextBoard(template)
+for i, d in enumerate(data):
+    stuff = step(model, data)
+    these_args = {}
+    if i % 100 == 0:
+        these_args['loss'] = stuff['loss'].detach()
+        these_args['score'] = stuff['loss'].detach()
+        these_args['probs'] = stuff['loss'].detach()
+    these_args['step'] = i
+    board.print(these_args)
+```
+
+and it will using the magic of eval it will print stuff like
+
+```
+Loss                                                  Score                                                
+▆▅▄▄▃▂▁                          ▁▂▃▄▅▅▆▇▇████████▇▇▆ ▆▅▄▄▃▂▁                          ▁▂▃▄▅▅▆▇▇████████▇▇▆
+████████▇▆▅▄▃▃▂▁▁▁▁▁▁▁▁▁▂▂▃▃▄▅▆▇█████████████████████ ████████▇▆▅▄▃▃▂▁▁▁▁▁▁▁▁▁▂▂▃▃▄▅▆▇█████████████████████
+
+
+Next letter probabilities
+              █                █                          █   ▃    ▁ ▃          
+▇▂▁▂▁▃▂▁▂▂▄▂▂▁█▁▇▁▁▄▁▂▁▃▁▃ ▅▃▁▃█▂▁▂▁▅▁▆▁▁▂█▁▂▂▂▂▂▂▁▃▁ ▄▂▃▄█▁▂▁██▁▁▄█▇█▁▃▆▄▁▂▂▄▅▁
+abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz
+    █   ▃    ▁ ▃             ▅               █                   █              
+▄▂▃▄█▁▂▁██▁▁▄█▇█▁▃▆▄▁▂▂▄▅▁ ▁▂█▁▃▅▂▃▅▁▂▁▂▁▁▁▁▅█▂▇▁▇▃▁▃ ▁▁▁▁▁▁▁▁▁▁▁█▁▂▁▁▁▁▁▂▁▁▁▁▁▁
+abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz
+
+Progress
+▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕▕ 16/100
+```
---
+++ textboard/__init__.py
@@ -0,0 +1,187 @@
+import os
+import re
+
+def get_terminal_width():
+    return os.get_terminal_size().columns
+
+def target_length(length):
+    return min(length or get_terminal_width(), get_terminal_width())
+
+def text(text, bold=False, length=None):
+    if bold:
+        return [f'\x1b[1m{text}\x1b[0m']
+    return [text]
+
+def resample(values, length):
+    output = []
+    for i in range(length):
+        output.append(values[int(i / length * len(values))])
+    return output
+
+boxes = "▁▂▃▄▅▆▇█"
+def sparklines(values, num_lines: int=2) -> list[str]:
+    # shift values to be positive
+    mn = min(values)
+    values = [v - mn for v in values]
+    mx = max(values)
+    mn = 0
+    segment_per_box = (mx - mn) / (num_lines * len(boxes))
+    lines = []
+    for i in range(num_lines - 1, -1, -1):
+        line = ''
+        for v in values:
+            value = v - i * segment_per_box * len(boxes)
+            if value < 0:
+                line += ' '
+            else:
+                line += boxes[min(len(boxes) - 1, int(value / segment_per_box))]
+        lines.append(line)
+    return lines
+
+def plot(values, height: int=4, title=None, length=None) -> list[str]:
+    length = target_length(length)
+    if length < len(values):
+        values = resample(values, length)
+    lines = sparklines(values, num_lines=height)
+    if title:
+        return text(title, bold=True, length=length) + lines
+    return lines
+
+def hist1d(values, height, labels=None, title=None, length=None) -> list[str]:
+    length = target_length(length)
+    if length < len(values):
+        raise ValueError("Length must be at least as long as the values")
+    lines = sparklines(values, num_lines=height)
+    if title:
+        lines = text(title, bold=True) + lines
+    if labels:
+        lines.append(''.join(labels))
+    return lines
+
+def progress(step, total, length=None):
+    text = f"{step}/{total}"
+    length = target_length(length)  - len(text) - 2
+    filled_parts = int(step / total * length)
+    bar = '▊' * filled_parts + '▕' * (length - filled_parts)
+    return [f"{bar} {step}/{total}"]
+
+def split(template):
+    split_regex = r"(\w+\(.*?\))\s*"
+    parts = []
+    for line in template.split('\n'):
+        parts.append(re.findall(split_regex, line))
+    return parts
+
+def no_terminal_crap_length(string):
+    return len(re.sub(r'\x1b\[[0-9;]*m', '', string))
+
+def render_template(template, args):
+    parts = split(template)
+    terminal_width = get_terminal_width() - 1 # :shrug emoji:
+    rendered_lines = []
+    for part in parts:
+        if not part:
+            rendered_lines.append([''])
+            continue
+
+        # Render
+        lines = []
+        target_length = terminal_width // len(part)
+        for func_call in part:
+            func_call = func_call.rstrip(')') + f', length={target_length})'
+            lines.append(eval(func_call, globals(), args))
+        lines = list(map(list, zip(*lines)))
+
+        # Check if horizontally stacked blocks are actually too long
+        need_flatten = False
+        for line in lines:
+            if sum(no_terminal_crap_length(l) for l in line) > terminal_width:
+                need_flatten = True
+                break
+        if need_flatten:
+            lines = list(map(list, zip(*lines)))
+            flat_lines = []
+            for line in lines:
+                flat_lines.extend([l] for l in line)
+            lines = flat_lines
+
+        # Padding
+        column_width = [0 for _ in lines[0]]
+        for line in lines:
+            for i, column in enumerate(line):
+                column_width[i] = max(column_width[i],
+                                      no_terminal_crap_length(column))
+        padded_lines = []
+        for line in lines:
+            padded_line = []
+            for i, column in enumerate(line):
+                padded_line.append(column + ' ' * (column_width[i] - no_terminal_crap_length(column)))
+            padded_lines.append(padded_line)
+        lines = padded_lines
+
+        rendered_lines.extend(lines)
+    return rendered_lines
+            
+
+
+class TextBoard:
+    def __init__(self, template):
+        self.template = template
+        self.args = {}
+        self.did_render_once = False
+
+    def print(self,
+              args,
+              extra_lines = [],
+              cleanup_previous = True):
+        if self.did_render_once and cleanup_previous:
+            for _ in range(self.last_length):
+                print('\033[F\033[K', end='')
+        self.args.update(args)
+        lines = render_template(self.template, self.args)
+        for line in lines + extra_lines:
+            print(' '.join(line))
+        self.did_render_once = True
+        self.last_length = len(lines)
+
+# import string
+# if __name__ == "__main__":
+#     import torch
+#     import time
+#     template = """plot(loss, title="Loss", height=2) plot(score, title="Score", height=2)
+
+
+#     text("Next letter probabilities", bold=True)
+#     hist1d(probs[0], height=2, labels=letters) hist1d(probs[1], height=2, labels=letters) hist1d(probs[3], height=2, labels=letters)
+#     hist1d(probs[3], height=2, labels=letters) hist1d(probs[4], height=2, labels=letters) hist1d(probs[5], height=2, labels=letters)
+
+#     text("Progress", bold=True)
+#     progress(step, total)"""
+
+#     letters = 'abcdefghijklmnopqrstuvwxyz'
+#     loss = torch.sin(torch.linspace(0, 2 * 3.14, 100)).numpy()
+#     score = torch.sin(torch.linspace(0, 2 * 3.14, 100)).numpy()
+#     logits = torch.randn(6, 26)
+#     probs = torch.softmax(logits, dim=-1).numpy()
+#     step = 10
+#     total = 100
+
+#     args = {
+#     'loss': 0,
+#     'score': 0,
+#     'probs': 0,
+#     'letters': string.ascii_lowercase,
+#     'step': 0,
+#     'total': len(data)
+#     }
+    
+#     board = TextBoard(template)
+#     for i, d in enumerate(data):
+#         # stuff = step(model, data)
+#         these_args = {}
+#         if i % 100 == 0:
+#             these_args['loss'] = stuff['loss'].detach()
+#             these_args['score'] = stuff['loss'].detach()
+#             these_args['probs'] = stuff['loss'].detach()
+#         these_args['step'] = i
+#         board.print(these_args)
